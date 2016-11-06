b77925e4bc69fad2d5e456a991b4944d0493f97d
Willy Tarreau <w@1wt.eu>
2016-11-06 19:38:13 +0100

    boot: add netsetup.lua
    This script sets the network up according to the wifi parameters, and
    switches to SOFTAP mode in case of failure (after 10 seconds). In this
    mode it sets the WiFi AP name to "iot4h-" followed by the chipid in
    decimal, and the password is "password" followed by the flashid. It
    serves DHCP by default so this allows to reconnect to the device over
    telnet and reconfigure it for example.

---
+++ boot/netsetup.lua
@@ -0,0 +1,31 @@
+wifi_retry=0
+wifi.setmode(wifi.STATION)
+if (wifi_sta_ip~=nil) then wifi.sta.setip({ip=wifi_sta_ip,netmask=wifi_sta_nm,gateway=wifi_sta_gw}) end
+wifi_sta_ip=nil wifi_sta_nm=nil wifi_sta_gw=nil
+if (wifi_sta_passwd==nil) then wifi_sta_passwd="" end
+if (wifi_sta_ssid~=nil) then
+  print("WiFi : trying " .. wifi_sta_ssid .. (wifi_sta_passwd == "" and " without password" or ""))
+  wifi.sta.config(wifi_sta_ssid,wifi_sta_passwd,1,wifi_sta_bssid)
+  wifi_retry=20
+end
+wifi_sta_ssid=nil wifi_sta_passwd=nil wifi_sta_bssid=nil
+
+tmr.alarm(0,500,tmr.ALARM_SEMI,function()
+  local status=wifi.sta.status()
+  local stnames={"idle","connecting","wrongpwd","apnotfound","fail","done"}
+  if status~=5 then
+    if wifi_retry > 0 then
+      print("WiFi : st=<" .. stnames[status+1] .. "> retrying #" .. wifi_retry)
+      wifi_retry=wifi_retry-1
+      tmr.start(0)
+    else
+      wifi.sta.disconnect() wifi.setmode(wifi.SOFTAP)
+      cfg={ssid="iot4h-"..node.chipid(), pwd="password"..node.flashid()}
+      print("WiFi : switching to AP. SSID="..cfg.ssid.." PWD="..cfg.pwd)
+      wifi.ap.config(cfg) cfg=nil wifi_retry=nil
+    end
+  else
+    print("WiFi connected: IP="..wifi.sta.getip().." MAC="..wifi.ap.getmac())
+    wifi_retry=nil
+  end
+end)
