b729f2d1c0530f9dbd42184769bad7e2ba7577c9
Willy Tarreau <w@1wt.eu>
2016-11-06 19:51:48 +0100

    boot: add init.lua
    It should be ready to deal with simple setups by now. It loads various
    config elements in turn, loads early what it finds in "lib/", and loads
    later what it finds in "svc/", then tries to load "appli.lc" and
    "appli.lua".

---
+++ boot/init.lua
@@ -0,0 +1,16 @@
+if file.exists("netconf.lua") then dofile("netconf.lua") end
+if file.exists("sysconf.lua") then dofile("sysconf.lua") end
+for n,s in pairs(file.list()) do
+  b,e = string.find(n, "lib/")
+  if b == 1 then print("Loading " .. n) dofile(n) end
+end
+if file.exists("netsetup.lua") then dofile("netsetup.lua") end
+if file.exists("socket.lua") then dofile("socket.lua") end
+if file.exists("pre-svc.lua") then dofile("pre-svc.lua") end
+for n,s in pairs(file.list()) do
+  b,e = string.find(n, "svc/")
+  if b == 1 then print("Loading " .. n) dofile(n) end
+end
+if file.exists("post-svc.lua") then dofile("post-svc.lua") end
+if file.exists("appli.lc") then dofile("appli.lc") end
+if file.exists("appli.lua") then dofile("appli.lua") end
