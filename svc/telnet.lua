ddeefed2977f0507c57af5eff4bd8fa2880e6f2b
Willy Tarreau <w@1wt.eu>
2016-11-13 10:51:39 +0100

    svc: telnet: implement local buffering
    After upgrading the modules to version 1.5.4, the telnet server stopped
    working. This issue is described in details in this comment :
    
      https://github.com/nodemcu/nodemcu-firmware/issues/730#issuecomment-154241161
    
    In short the problem is that net.socket:send() doesn't implement EAGAIN
    and used to buffer everything and to cause memory outages. Now it doesn't
    do that anymore and it's up to the application to do it instead (which is
    not much better). The issue above suggests that implementing a local buffer
    and manipulating it via table.insert() and table.remove() is the most optimal
    solution avoiding expensive memory copies. This is what this commit does.
    Please note that table.insert() is misnamed, as it appends in reality.

--- svc/telnet.lua
+++ svc/telnet.lua
@@ -1,11 +1,29 @@
 tcpsrv:listen(2323,function(c)
+  local sending=false
+  local buffer={}
+
+  function sent(c)
+    sending=false
+    if #buffer > 0 then
+      c:send(table.remove(buffer,1))
+      sending=true
+    end
+  end
+
   function s_output(str)
-    if c~=nil then c:send(str) end
+    if c==nil then return end
+    if sending then
+      table.insert(buffer,str)
+    else
+      c:send(str)
+      sending=true
+    end
   end
   node.output(s_output, 0)
   c:on("receive",function(c,l)
     node.input(l)
   end)
+  c:on("sent",sent)
   c:on("disconnection",function(c)
     node.output(nil)
   end)
