e8cc701e39a088bc0c3da147c2e639ca10fefc58
Willy Tarreau <w@1wt.eu>
2016-11-13 12:45:19 +0100

    svc: telnet: implement password-based protection
    Now the first connection is responsible for authenticating. A password
    is asked for. As long as it fails, the password is asked for again. Once
    the password is valid, the shell is granted, and new connections can
    enter in parallel without being asked anything.
    
    The first line indicates the chipid, the IP address, and if the service is
    currently authenticated or anonymous. Until authenticated, extra connections
    are simply rejected.
    
    The telnet password is found in variable "telnet_passwd" which should be
    set in sysconf.lua. If it is not found, then the password defaults to the
    concatenation of the chipid, the flashid and the flashsize, all verbatim
    as reported by node.*() and printed in decimal without any delimiter. At
    least it minimally protects against random connections. The port and
    password are printed on the serial port at boot time.

--- svc/telnet.lua
+++ svc/telnet.lua
@@ -1,4 +1,10 @@
 local telnet_conns=0
+local telnet_auth=false
+
+if telnet_passwd==nil then
+  telnet_passwd="" .. node.chipid() .. node.flashid() ..node.flashsize()
+end
+print("Starting telnet on port " .. 2323 .. " with password [" .. telnet_passwd .."]")
 tcpsrv:listen(2323,function(c)
   local sending=false
   local buffer={}
@@ -11,12 +17,6 @@ tcpsrv:listen(2323,function(c)
       sending=true
     end
   end
-  node.output(s_output, 0)
-  c:on("receive",function(c,l)
-    c:hold()
-    node.input(l)
-    c:unhold()
-  end)
   c:on("sent",function(c)
     sending=false
     if #buffer > 0 then
@@ -26,8 +26,42 @@ tcpsrv:listen(2323,function(c)
   end)
   c:on("disconnection",function(c)
     telnet_conns=telnet_conns-1
-    node.output(nil)
+    if telnet_auth then
+      node.output(nil)
+    end
+    if telnet_conns == 0 then
+      telnet_auth=false
+    end
   end)
+
+  c:on("receive",function(c,l)
+    if telnet_auth then
+      c:hold()
+      node.input(l)
+      c:unhold()
+    else
+      local pass=l:gsub("\r",""):gsub("\n","")
+      if pass == telnet_passwd then
+        telnet_auth=true
+        s_output("> ");
+        node.output(s_output, 0)
+      else
+        s_output("Wrong password, try again.\nPassword: ");
+      end
+    end
+  end)
+
   telnet_conns=telnet_conns+1
-  print("[" .. ((wifi.sta.getip()~=nil) and wifi.sta.getip() or wifi.ap.getip()) .. " - " .. telnet_conns .. "]\n> ")
+  s_output("[" ..
+    string.format("%x",node.chipid()) .. "/" ..
+    ((wifi.sta.getip()~=nil) and wifi.sta.getip() or wifi.ap.getip()) .. " - " ..
+    telnet_conns .. ":" ..
+    (telnet_auth and "AUTH" or "ANON") .. "]\n" ..
+    ((telnet_auth and "> ") or ((telnet_conns == 1) and "Password: ") or "Authenticate first!\n"))
+
+  if (telnet_auth) then
+    node.output(s_output, 0)
+  elseif telnet_conns > 1 then
+    c:close()
+  end
 end)
