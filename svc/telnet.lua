90b8ee5fd24ba38f32a9b0027ef62a7afabe974e
Willy Tarreau <w@1wt.eu>
2016-11-13 11:13:54 +0100

    svc: telnet: use local functions for all callbacks
    We don't want these functions names to leak outside.

--- svc/telnet.lua
+++ svc/telnet.lua
@@ -1,16 +1,7 @@
 tcpsrv:listen(2323,function(c)
   local sending=false
   local buffer={}
-
-  function sent(c)
-    sending=false
-    if #buffer > 0 then
-      c:send(table.remove(buffer,1))
-      sending=true
-    end
-  end
-
-  function s_output(str)
+  local function s_output(str)
     if c==nil then return end
     if sending then
       table.insert(buffer,str)
@@ -25,7 +16,13 @@ tcpsrv:listen(2323,function(c)
     node.input(l)
     c:unhold()
   end)
-  c:on("sent",sent)
+  c:on("sent",function(c)
+    sending=false
+    if #buffer > 0 then
+      c:send(table.remove(buffer,1))
+      sending=true
+    end
+  end)
   c:on("disconnection",function(c)
     node.output(nil)
   end)
