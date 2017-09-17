21a96c9bba9a646f9738fd937ad73231076d9a32
Willy Tarreau <w@1wt.eu>
2017-09-17 12:52:35 +0200

    boot: init: sort the files from lib/ and svc/
    This will allow to have certain services depend on each other.

--- boot/init.lua
+++ boot/init.lua
@@ -1,20 +1,32 @@
+function load_dir(dir)
+  local toload={}
+  for n,s in pairs(file.list()) do
+    b,e = string.find(n, dir .. "/")
+    if b == 1 then table.insert(toload,n) end
+  end
+  table.sort(toload)
+
+  for n,f in pairs(toload) do
+    print("Loading " .. f)
+    dofile(f)
+  end
+end
+
 if file.exists("nodemcu.lua") then dofile("nodemcu.lua") end
 if file.exists("firmware.lua") then dofile("firmware.lua") end
 if file.exists("board.lua") then dofile("board.lua") end
 
 if file.exists("netconf.lua") then dofile("netconf.lua") end
 if file.exists("sysconf.lua") then dofile("sysconf.lua") end
-for n,s in pairs(file.list()) do
-  b,e = string.find(n, "lib/")
-  if b == 1 then print("Loading " .. n) dofile(n) end
-end
+
+load_dir("lib")
+
 if file.exists("netsetup.lua") then dofile("netsetup.lua") end
 if file.exists("socket.lua") then dofile("socket.lua") end
 if file.exists("pre-svc.lua") then dofile("pre-svc.lua") end
-for n,s in pairs(file.list()) do
-  b,e = string.find(n, "svc/")
-  if b == 1 then print("Loading " .. n) dofile(n) end
-end
+
+load_dir("svc")
+
 if file.exists("post-svc.lua") then dofile("post-svc.lua") end
 if file.exists("appli.lc") then dofile("appli.lc") end
 if file.exists("appli.lua") then dofile("appli.lua") end
