924cb8efb6110fe3d04412566a468f9b3482c981
Willy Tarreau <w@1wt.eu>
2017-09-17 12:48:54 +0200

    lib: make cat() read the entire file
    Till now it used to only show the first block.

--- lib/cat.lua
+++ lib/cat.lua
@@ -1,1 +1,7 @@
-function cat(n) if file.open(n,"r") then print(file.read()); file.close(); end end
+function cat(n)
+  local f = n and file.open(n,"r")
+  while f ~= nil do
+    local p = file.read(f)
+    if not p then file.close(f) f=nil else print(p) end
+  end
+end
