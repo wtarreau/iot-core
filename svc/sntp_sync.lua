7986025c39d0a4b680f25dace312b6f9afc6d2d7
Willy Tarreau <w@1wt.eu>
2017-09-17 14:42:32 +0200

    svc: add sntp_sync.lua
    This one periodically retrieves the current date over ntp. However it's
    not performed at boot time because a bug in the implementation in version
    1.5.4 makes it crash if the WiFi is not up and ready. So for now we only
    export an sntp_sync() function.

---
+++ svc/sntp_sync.lua
@@ -0,0 +1,10 @@
+function sntp_sync()
+  if sntp_srv_ip ~= nil then
+    sntp.sync(sntp_srv_ip, function()
+      sntp_sync_state=1
+      sntp_last_sync=tmr.time()
+    end, function()
+      sntp_sync_state=0
+    end, 1)
+  end
+end
