9835dfd0ecb0e6826f24b5689639f29a3140c4bc
Willy Tarreau <w@1wt.eu>
2017-09-23 23:26:33 +0200

    boot: firmware: also support font font_04b_03r for displays
    It's slightly smaller than the other one.

--- firmware/firmware-1.5.4.1.lua
+++ firmware/firmware-1.5.4.1.lua
@@ -1,3 +1,3 @@
 -- firmware-specific stuff, that may change with firmware builds
-DISP_FONT=u8g and (u8g.font_04b_03 or u8g.font_5x8 or u8g.font_6x10 or u8g.font_chikita)
+DISP_FONT=u8g and (u8g.font_04b_03r or u8g.font_04b_03 or u8g.font_5x8 or u8g.font_6x10 or u8g.font_chikita)
 I2C_SPEED=i2c and i2c.SLOW
