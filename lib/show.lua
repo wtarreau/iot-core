0a840a60daaed5ad644490ce52f68e7e5b847df4
Willy Tarreau <w@1wt.eu>
2018-11-04 12:43:11 +0100

    lib: show: implement sshow() which is a sorted version of show()
    This is usable for small tables, it duplicates the table locally into
    an indexed table that it then dumps. It's convenient when looking for
    a function name in a module or for a file name.

--- lib/show.lua
+++ lib/show.lua
@@ -1,1 +1,7 @@
 function show(x) for k,v in pairs(x) do print(k,v) end end
+function sshow(x)
+  local t = {}
+  for k in pairs(x) do table.insert(t, k) end
+  table.sort(t)
+  for i,k in ipairs(t) do print(k,x[k]) end
+end
