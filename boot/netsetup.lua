e330a3c162b740435b0542a04167012f6c20227b
Willy Tarreau <w@1wt.eu>
2019-12-25 17:00:46 +0100

    boot: make netsetup compatible with newer timer API
    The older timer API supporting an integer timer ID is not supported
    anymore, and alarm() isn't part of tmr anymore but of the returned
    timer. So let's use tmr.create() and nothing else.

--- boot/netsetup.lua
+++ boot/netsetup.lua
@@ -1,7 +1,12 @@
-wifi_retry=0
+local wifi_retry_timer
+local wifi_retry=0
+
 wifi.setmode(wifi.STATION)
+
 if (wifi_sta_ip~=nil) then wifi.sta.setip({ip=wifi_sta_ip,netmask=wifi_sta_nm,gateway=wifi_sta_gw}) end
+
 wifi_sta_ip=nil wifi_sta_nm=nil wifi_sta_gw=nil
+
 if (wifi_sta_passwd==nil) then wifi_sta_passwd="" end
 if (wifi_sta_ssid~=nil) then
   print("WiFi : trying " .. wifi_sta_ssid .. (wifi_sta_passwd == "" and " without password" or ""))
@@ -14,16 +19,18 @@ if (wifi_sta_ssid~=nil) then
   end
   wifi_retry=20
 end
+
 wifi_sta_ssid=nil wifi_sta_passwd=nil wifi_sta_bssid=nil
 
-tmr.alarm(0,500,tmr.ALARM_SEMI,function()
+wifi_retry_timer=tmr.create()
+wifi_retry_timer:alarm(500,tmr.ALARM_SEMI,function()
   local status=wifi.sta.status()
   local stnames={"idle","connecting","wrongpwd","apnotfound","fail","done"}
   if status~=5 then
     if wifi_retry > 0 then
       print("WiFi : st=<" .. stnames[status+1] .. "> retrying #" .. wifi_retry)
       wifi_retry=wifi_retry-1
-      tmr.start(0)
+      wifi_retry_timer:start()
     else
       wifi.sta.disconnect() wifi.setmode(wifi.SOFTAP)
       local cfg={ssid="iot4h-"..node.chipid(), pwd="password"..node.flashid()}
@@ -37,6 +44,8 @@ tmr.alarm(0,500,tmr.ALARM_SEMI,function()
     end
   else
     print("WiFi connected: IP="..wifi.sta.getip().." MAC="..wifi.ap.getmac())
+    wifi_retry_timer:unregister()
+    wifi_retry_timer=nil
     wifi_retry=nil
     if sntp_sync ~= nil and (not recovery or recovery() == 0) then sntp_sync() end
   end
