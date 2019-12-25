b1b44e1069bee4a70c09c56862a13a36fcff2936
Willy Tarreau <w@1wt.eu>
2019-12-25 20:40:25 +0100

    lib: clean up the xxd function
    Using an array instead of building the string on the fly costs quite
    some bytes in the function. Let's construct the string by concatenation
    instead to shrink the function by ~25%. Local variables were placed
    closer to their usage to save a few bytes, and the code was slightly
    cleaned up to make it more readable.

--- lib/xxd.lua
+++ lib/xxd.lua
@@ -1,17 +1,21 @@
 function xxd(fn)
-  local f,d,b,c,o; local h={}; local p=0
-  f=file.open(fn,"rb")
-  while f~=nil do
-    d=f:read(16); o=""
-    for b=1,16 do
-      if b<=#d then
-        c=d:byte(b); h[b]=string.format('%02x ',c)
-        if (c>=32 and c<=127) then o=o..d:sub(b,b) else o=o.."." end
+  local p,f = 0, file.open(fn, "rb")
+  while f ~= nil do
+    local o,h,d = "", "", f:read(16)
+    for b = 1,16 do
+      if b <= #d then
+        local k = d:sub(b,b)
+        local c = k:byte()
+	h = h .. string.format('%02x ', c)
+        if (c < 32 or c > 127) then k = "." end
+        o = o .. k
       else
-        h[b]="   "; file.close(f); f=nil
+        h = h .. "   "
+	file.close(f)
+	f = nil
       end
     end
-    print(string.format('%04x: ',p)..h[1]..h[2]..h[3]..h[4]..h[5]..h[6]..h[7]..h[8]..h[9]..h[10]..h[11]..h[12]..h[13]..h[14]..h[15]..h[16].." "..o)
-    p=p+16
+    print(string.format('%04x: ', p) .. h .. " " .. o)
+    p = p + 16
   end
 end
