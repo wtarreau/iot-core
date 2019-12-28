7601ea8db92dc47ec4d9f41a46346c0fcb87788d
Willy Tarreau <w@1wt.eu>
2019-12-28 16:50:32 +0100

    lib: make xxd suitable for use in LFS
    It's now a local function exported to global symbols. It was also commented
    in order to be easier to reuse.

--- lib/split.lua
+++ lib/split.lua
@@ -1,1 +1,13 @@
-function split(s, d) local i=1; local r={}; for m in string.gmatch(s,"([^"..d.."]+)") do r[i]=m i=i+1 end return r end
+-- Returns an array containing all parts of string <s> cut around any of the
+-- delimiting characters in string <d>. This is useful to cut IP addresses or
+-- MAC addresses around '.' and ':' for example.
+local function split(s, d)
+  local r = {}
+  for m in string.gmatch(s,"([^"..d.."]+)") do
+    r[#r+1]=m
+  end
+  return r
+end
+
+local G=getfenv()
+G.split=split
