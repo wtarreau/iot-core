eb7d2007023565ca9d715420bcc2397986c5b238
Willy Tarreau <w@1wt.eu>
2019-12-31 15:44:04 +0100

    boot: split init.lua into "init" and "start"
    With LFS it becomes more convenient to think in terms of initialization
    step and of service loader. This also allows to further shrink the init
    code to the minimum needed to flash+upgrade a new LFS image, or load the
    existing one.
    
    "start" is the service startup sequence. It continues to look up certain
    files and to load certain directories. It also supports this with LFS,
    though files should be prefixed with "lib_" or "svc_" instead of "lib/"
    and "svc/" in order to be automatically loaded.
    
    Since there is backwards compatibility with previous init code, the old
    init was replaced with the new one. It will just silently ignore anything
    LFS-related if LFS is not supported.

--- boot/init.lua
+++ boot/init.lua
@@ -18,52 +18,18 @@ end
 -- remove leftover from a previous upgrade
 file.remove("lfs.tmp")
 
-if node.flashindex then pcall(function() node.flashindex'_init'() end) end
-
-
-local function load_dir(dir)
-  local toload={}
-  for n,s in pairs(file.list()) do
-    local b,e = string.find(n, dir .. "/")
-    if b == 1 then table.insert(toload,n) end
-  end
-  table.sort(toload)
-
-  for n,f in pairs(toload) do
-    print("Loading " .. f)
-    dofile(f)
-  end
+-- load all LFS symbols
+if node.flashindex and not pcall(function() node.flashindex'_init'() end) then
+  print("Caught exception while initializing LFS")
 end
 
-local function load_file(f)
-  if file.exists(f .. ".lc") then dofile(f .. ".lc")
-  elseif file.exists(f .. ".lua") then dofile(f .. ".lua")
+-- start, either from LFS or from SPIFFS
+if file.exists("start.lc") or LFS.start ~= nil then
+  if not pcall(dofile,"start.lc") then
+    print("Caught exception while initializing services")
+  end
+elseif file.exists("start.lua") then
+  if not pcall(dofile,"start.lua") then
+    print("Caught exception while initializing services")
   end
 end
-
-load_file("nodemcu")
-load_file("firmware")
-load_file("board")
-load_file("netconf")
-load_file("sysconf")
-
-load_dir("lib")
-
-load_file("netsetup")
-load_file("socket")
-load_file("pre-svc")
-
-load_dir("svc")
-
-load_file("post-svc")
-
--- release memory
-load_dir=nil
-
-if not recovery or recovery() == 0 then
-  load_file("appli")
-else
-  print("Skipping appli.lua due to recovery button")
-end
-
-load_file=nil
---
+++ boot/start.lua
@@ -0,0 +1,55 @@
+local function load_dir(dir)
+  local toload={}
+  for n,s in pairs(file.list()) do
+    local b,e = string.find(n, dir .. "/")
+    if b == 1 then table.insert(toload,n) end
+  end
+
+  if LFS and LFS._list then
+    for i,n in pairs(LFS._list()) do
+      local b,e = string.find(n, dir .. "_")
+      if b == 1 then table.insert(toload,n .. ".lc") end
+    end
+  end
+
+  table.sort(toload)
+
+  for n,f in pairs(toload) do
+    print("Loading " .. f)
+    dofile(f)
+  end
+end
+
+local function load_file(f)
+  if file.exists(f .. ".lc") or type(LFS[f]) == "function" then dofile(f .. ".lc")
+  elseif file.exists(f .. ".lua") then dofile(f .. ".lua")
+  end
+end
+
+load_file("nodemcu")
+load_file("firmware")
+load_file("board")
+load_file("netconf")
+load_file("sysconf")
+
+load_dir("lib")
+
+load_file("netsetup")
+load_file("socket")
+load_file("pre-svc")
+
+load_dir("svc")
+
+load_file("post-svc")
+
+-- release memory
+load_dir=nil
+
+if not recovery or recovery() == 0 then
+  print("Loading appli.lua")
+  load_file("appli")
+else
+  print("Skipping appli.lua due to recovery button")
+end
+
+load_file=nil
